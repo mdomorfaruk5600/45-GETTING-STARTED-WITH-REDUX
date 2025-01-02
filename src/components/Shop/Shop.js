6bbc72ca9797488bd604add06d1f746a526320c1
mdomorfaruk5600 <md.omorfaruk5600@gmail.com>
2025-01-02 11:47:12 +0600

    45-7 connect component with redux state and actions

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Product from '../Product/Product';
+import { addToCart } from './../../redux/actions/cartActions';
+import { connect } from 'react-redux';
 
-const Shop = () => {
+const Shop = (props) => {
     const products = [
         {name:"lenovo", id:1},
         {name:"Asus", id:2},
@@ -9,9 +11,10 @@ const Shop = () => {
         {name:"Dell", id:4},
         {name:"Hp", id:5},
     ]
+    console.log(props);
     return (
         <div>
-            <h2>This is Shop.</h2>
+            <h2 onClick={()=>addToCart(1)}>This is Shop.</h2>
             {
                 products.map(pd => <Product key={pd.id} product={pd} ></Product>)
             }
@@ -19,4 +22,19 @@ const Shop = () => {
     );
 };
 
-export default Shop;
+const mapStateToProps = state => {
+    console.log(state);
+    return {
+        cart: state.carts,
+    }
+}
+
+const mapDispatchToProps = {
+    addToCart:addToCart,
+}
+
+// const connectToStore = connect(mapStateToProps, mapDispatchToProps);
+
+// connectToStore(Shop)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Shop);
